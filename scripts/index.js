ccf8fd0a29f0e67b32959470700259ddd461924a
Nicolas Cordoba <nicolascordoba@nicolass-mbp.mynetworksettings.com>
2024-08-11 20:11:10 -0700

    new update

--- scripts/index.js
+++ scripts/index.js
@@ -33,9 +33,9 @@ const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
 
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal__edit");
+  profileEditModal.classList.add("modal");
 });
 
 profileEditCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__edit");
+  profileEditModal.classList.remove("modal");
 });
